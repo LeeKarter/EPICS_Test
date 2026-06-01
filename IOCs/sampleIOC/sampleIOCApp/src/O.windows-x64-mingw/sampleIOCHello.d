ea8a0ffed2375021b0f846319f77e50836f63a48
LeeKarter <156885097+LeeKarter@users.noreply.github.com>
2026-06-01 08:42:13 +0300

    Add files via upload

---
+++ IOCs/sampleIOC/sampleIOCApp/src/O.windows-x64-mingw/sampleIOCHello.d
@@ -0,0 +1,7 @@
+sampleIOCHello.obj: ../sampleIOCHello.c \
+ E:/MSYStest/home/LeeKarter/base-7.0.10/include/epicsExport.h \
+ E:/MSYStest/home/LeeKarter/base-7.0.10/include/shareLib.h \
+ E:/MSYStest/home/LeeKarter/base-7.0.10/include/iocsh.h \
+ E:/MSYStest/home/LeeKarter/base-7.0.10/include/compilerDependencies.h \
+ E:/MSYStest/home/LeeKarter/base-7.0.10/include/compiler/gcc/compilerSpecific.h \
+ E:/MSYStest/home/LeeKarter/base-7.0.10/include/libComAPI.h
